e3be43d9c910552e520634dd564b5e1b5bdc91e2
mustufain.abbas <mustufain.abbas@caidio.io>
2018-12-01 01:29:42 +0200

    item to item based collaborative filtering updated.

--- item_item_collaborative_filtering.py
+++ item_item_collaborative_filtering.py
@@ -1,31 +1,33 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import pairwise_distances
-# https://towardsdatascience.com/how-to-build-a-simple-song-recommender-296fcbc8c85
-# https://www.analyticsvidhya.com/blog/2018/06/comprehensive-guide-recommendation-engine-python/
+
+""""
+Personalized Recommendations for every user.
+Recommendations for user[10]:
+
+4074                              Be With You - Beyoncé
+1426                  Relax - Frankie Goes To Hollywood
+2234                            Home - Three Days Grace
+1270                      Paradise City - Guns N' Roses
+878       The Only Exception (Album Version) - Paramore
+4069    Check On It - Beyoncé feat. Bun B and Slim Thug
+661                   What They Found - Octopus Project
+209                                Wet Blanket - Metric
+3735             Oxford Comma (Album) - Vampire Weekend
+388      Hippie Priest Bum-out (Edit) - LCD Soundsystem
+"""
 
 
 class MusicRecommender(object):
+    """Item - Item based collaborative filtering."""
 
     def __init__(self):
+        self.target = None
+        self.datamatrix = None
 
-        triplets_file = 'https://static.turi.com/datasets/millionsong/10000.txt'
-        songs_metadata_file = 'https://static.turi.com/datasets/millionsong/song_data.csv'
-        song_df_1 = pd.read_table(triplets_file, header=None)
-        song_df_1.columns = ['user_id', 'song_id', 'listen_count']
-        # Read song metadata
-        song_df_2 = pd.read_csv(songs_metadata_file)
-        # Merge the two dataframes above to create input dataframe for recommender systems
-        song_df = pd.merge(song_df_1, song_df_2.drop_duplicates(
-            ['song_id']), on="song_id", how="left")
-        song_df = song_df.head(10000)
-        self.target = song_df
-        self.target['song_name'] = self.target.apply(
-            lambda row: str(row['title']) + " - " + str(row['artist_name']),
-            axis=1)
-
-    def popularity_based_recommendation(self):
-        return
+    def create(self, data):
+        self.target = data
 
     def get_unique_users(self):
         users = self.target.user_id.unique()
@@ -56,14 +58,14 @@ class MusicRecommender(object):
         item_similarity = pairwise_distances(data_matrix.T, metric='cosine')
         return item_similarity
 
-    def create_user_songs_matrix(self):
+    def fit(self):
         data_matrix = np.zeros((len(self.get_unique_users()),
                                 len(self.get_unique_songs())))
         user_lookup = self.user_lookup()
         song_lookup = self.song_lookup()
         for line in self.target.itertuples():
             data_matrix[user_lookup[line[1]], song_lookup[line[2]]] = line[3]
-        return data_matrix
+        self.datamatrix = data_matrix
 
     def predict_user(self, data_matrix):
         user_similarity = self.get_user_similarity(data_matrix)
@@ -80,22 +82,28 @@ class MusicRecommender(object):
             [np.abs(item_similarity).sum(axis=1)])
         return pred
 
-    def recommend_item(self, data_matrix, user_id):
+    def recommend(self, user_id):
         """Personalized top 10 recommendation for each user"""
-        pred = self.predict_item(data_matrix)
+        pred = self.predict_item(self.datamatrix)
         pred_transpose = pd.DataFrame(pred.T)
         user = self.user_lookup()[user_id]
         user_songs = pred_transpose[user]
         sorted_user_songs = user_songs.sort_values(ascending=False)
         top_10_songs_list = list(sorted_user_songs.index)[0:10]
-        print ("----------Recommending top 10 songs for user_id" + str(
+        print ("----------Recommending top 10 songs for user_id " + str(
             user_id) + "----------")
         print (self.target.loc[top_10_songs_list]['song_name'])
 
 
 if __name__ == '__main__':
 
-    user_id = "4bd88bfb25263a75bbdd467e74018f4ae570e5df"
+    song_df = pd.read_csv('data/songs.csv')
+    song_df['song_name'] = song_df.apply(
+        lambda row: str(row['title']) + " - " + str(row['artist_name']),
+        axis=1)
+    users = song_df['user_id'].unique()
+    user_id = users[10]
     music = MusicRecommender()
-    user_song_matrix = music.create_user_songs_matrix()
-    music.recommend_item(user_song_matrix, user_id)
+    music.create(song_df)
+    music.fit()
+    music.recommend(user_id)
--- popularity_based_recommender.py
+++ popularity_based_recommender.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 """
 Information about data:
@@ -14,6 +13,19 @@ songs subset of 10000 songs
 
 Note : These are not personalized recommendations. For every user,
 recommendation will be same as it recommends top 10 trending songs.
+
+Recommendation for user[10]:
+
+7127                             Sehr kosmisch - Harmonia
+9084                                         Undo - Björk
+2068    Dog Days Are Over (Radio Edit) - Florence + Th...
+9880                       You're The One - Dwight Yoakam
+6774                              Revelry - Kings Of Leon
+7115                                Secrets - OneRepublic
+3613    Horn Concerto No. 4 in E flat K495: II. Romanc...
+2717                       Fireflies - Charttraxx Karaoke
+3485                             Hey_ Soul Sister - Train
+8847                                   Tive Sim - Cartola
 """
 
 
@@ -21,20 +33,21 @@ class PopulairtyRecommender(object):
     """Recommending most popular/trending songs"""
 
     def __init__(self):
-        self.train_data = None
+        self.data = None
         self.popularity_recommendations = None
 
     # Create the popularity based recommender system model
-    def create(self, train_data):
-        self.train_data = train_data
+    def create(self, data):
+        self.data = data
 
+    def fit(self):
         # Get a count of user_ids for each unique song as recommendation score
-        train_data_grouped = train_data.groupby(
-            'song')['user_id'].count().reset_index()
-        train_data_grouped.columns = ['song', 'score']
+        train_data_grouped = self.data.groupby(
+            'song_name')['user_id'].count().reset_index()
+        train_data_grouped.columns = ['song_name', 'score']
         # Sort the songs based upon recommendation score
         train_data_sort = train_data_grouped.sort_values(
-            ['score', 'song'], ascending=[0, 1])
+            ['score', 'song_name'], ascending=[0, 1])
         # Generate a recommendation rank based upon score
         train_data_sort['Rank'] = train_data_sort['score'].rank(
             ascending=0, method='first')
@@ -49,20 +62,21 @@ class PopulairtyRecommender(object):
         cols = user_recommendations.columns.tolist()
         cols = cols[-1:] + cols[:-1]
         user_recommendations = user_recommendations[cols]
+        print ("----------Recommending top 10 songs for user_id " + str(
+            user_id) + "----------")
+        print (user_recommendations['song_name'])
         return user_recommendations
 
 
 if __name__ == '__main__':
 
     song_df = pd.read_csv('data/songs.csv')
+    song_df['song_name'] = song_df.apply(
+        lambda row: str(row['title']) + " - " + str(row['artist_name']),
+        axis=1)
     users = song_df['user_id'].unique()
-    train_data, test_data = train_test_split(
-        song_df, test_size=0.20, random_state=0)
-    pr = PopulairtyRecommender()
-    pr.create(train_data)
-    # user the popularity model to make some prediction
     user_id = users[10]
-    user_recommendations = pr.recommend(user_id)
-    print ("----------Recommending top 10 songs for user_id " + str(
-        user_id) + "----------")
-    print (user_recommendations['song'])
+    pr = PopulairtyRecommender()
+    pr.create(song_df)
+    pr.fit()
+    pr.recommend(user_id)
